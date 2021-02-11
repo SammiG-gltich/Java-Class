d5a1fdf93fb646d2d5e4239311f95c2d9f4f29de
SammiG <gervse@farmingdale.edu>
2021-02-10 21:19:50 -0500

    All Done

--- Lab Assignment 1/src/lab/assignment/pkg1/LabAssignment1.java
+++ Lab Assignment 1/src/lab/assignment/pkg1/LabAssignment1.java
@@ -1,8 +1,8 @@
-
 package lab.assignment.pkg1;
 
 /**
  * Just working through Git first.
+ *
  * @author Sammi 2/10/2021
  */
 public class LabAssignment1 {
@@ -11,13 +11,15 @@ public class LabAssignment1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         for (int i = 0; i <= 100; i++) {
+
+        for (int i = 0; i <= 100; i++) {
+            //initialize i and make it equal to 0. run the program to check if i is even and then add 1 to i and run it again until i=100
             if (i % 2 == 0) {
                 // if i mod 2 has no remainder, then print i
                 System.out.println(i);
             }
         }
-        
+
     }
-    
+
 }
