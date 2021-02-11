e1d78145654dab02b5d964fe4075b7e192b5be5b
SammiG <gervse@farmingdale.edu>
2021-02-10 21:11:18 -0500

    Making the for loop and testing it .

--- Lab Assignment 1/src/lab/assignment/pkg1/LabAssignment1.java
+++ Lab Assignment 1/src/lab/assignment/pkg1/LabAssignment1.java
@@ -11,8 +11,8 @@ public class LabAssignment1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         for (int i = 0; i <= 20;) {
-            if (i % 2 != 1) {
+         for (int i = 0; i <= 100;) {
+            if (i % 2 == 0) {
                 System.out.println(i);
             }
             i++;
