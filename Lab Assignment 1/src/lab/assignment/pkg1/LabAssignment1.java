58f24ae3d3ee805ffa38321d496643d6ceb8fb82
SammiG <gervse@farmingdale.edu>
2021-02-10 21:15:39 -0500

    adding the commenting

--- Lab Assignment 1/src/lab/assignment/pkg1/LabAssignment1.java
+++ Lab Assignment 1/src/lab/assignment/pkg1/LabAssignment1.java
@@ -11,11 +11,11 @@ public class LabAssignment1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         for (int i = 0; i <= 100;) {
+         for (int i = 0; i <= 100; i++) {
             if (i % 2 == 0) {
+                // if i mod 2 has no remainder, then print i
                 System.out.println(i);
             }
-            i++;
         }
         
     }
