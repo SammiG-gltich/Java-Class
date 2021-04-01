9875925d824d42a2cd2c10244be1d10e6c23db88
SammiG <gervse@farmingdale.edu>
2021-03-31 21:11:52 -0400

    abstract methods demo pt2

--- Week9Demo/src/week9demo/Week9Demo.java
+++ Week9Demo/src/week9demo/Week9Demo.java
@@ -1,33 +1,54 @@
 
 package week9demo;
 
-public class Week9Demo extends Student implements StudentInter{
 
-    int getAge(){
-        return age;
-    }
+import java.util.ArrayList;
+import java.util.List;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.shape.*;
+import javafx.scene.shape.Shape;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Week9Demo extends Application{
+
     public static void main(String[] args) {
-
-        Week9Demo w9 = new Week9Demo();
-        System.out.println("age is " + w9.getAge());
+        launch(args);
     }
 
-    @Override
-    public void setAge() {
-    }
-
-    @Override
-    void setId() {
-        
-    }
     
-}
-abstract class Student{// if you have an abstract class you dont neccesarily need a abstract method
-     
-    int age = 12;
-    abstract void setId();//however, if you have an abstract method you must have an abstract class
- }
-
-interface StudentInter{
-    void setAge();
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Shape shp;
+        shp = new Circle(400,400,50);
+        
+        Label lbl = new Label("Text here");
+        List<Shape> lst = new ArrayList<Shape>();
+        lst.add( new Text(50,50,"Samantha G"));
+        lst.add( new Rectangle(80,80,200,200));
+        lst.add( new Line(150,150,400,400));
+        
+        Control[] arrCrt = new Control[5];
+            arrCrt[0]= new TextField();
+            arrCrt[1] = new Button();
+        
+                
+        Group gp = new Group(shp, lbl);
+        
+        for(Shape s: lst){
+            gp.getChildren().add(s);
+        }
+        
+        for(Control s: arrCrt){
+           // gp.getChildren().add(s);
+        }
+        
+        stage.setScene(new Scene(gp,500,400));
+        stage.show();
+    }
 }
